f43068703e5dabb1108c97abf809dbbcc34cac2e
Brenda Cano <bcano@emory.edu>
2021-01-02 12:05:54 -0500

    organized into methods/functions

--- backend/organize.py
+++ backend/organize.py
@@ -4,61 +4,40 @@ from oauth2client.service_account import ServiceAccountCredentials
 from pprint import pprint
 
 scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
-
-creds = ServiceAccountCredentials.from_json_keyfile_name('python\creds.json', scope)
-
+creds = ServiceAccountCredentials.from_json_keyfile_name('backend\creds.json', scope)
 client = gspread.authorize(creds)
 
-#access google sheets database
-database = client.open("organizedData").get_worksheet(2)
-
-data = database.get_all_records()
-
-pprint(data)
-
-#access txtfile
-#f = open('python\list.txt',"r")
-
-#data = f.read()
-
-#parse txtfile
+def request():
+    #access google sheets database
+    database = client.open("organizedData").get_worksheet(2)
+    data = database.get_all_records()
+    return data
 
 #-counters
-#-------recyclable items
-recycle =  sum([1 for i in range(len(data)) if data[i]["Classification"] == 'recycable'])
-
-#-------compostable items
-compost = sum([1 for i in range(len(data)) if data[i]["Classification"] == 'organic'])
-
-#-------landfill items
-landfill = sum([1 for i in range(len(data)) if data[i]["Classification"] == 'trash'])
-
-
-#print items
-#-------recyclable items
-print('recycable:', recycle)
-
-#-------compostable items
-print('compostable:', compost)
-
-#-------landfill items
-print('landfill:', landfill)
-
-#list
-alldata = []
-alldata.append(recycle)
-alldata.append(compost)
-alldata.append(landfill)
-
-print(alldata)
+def count(data) :
+    #recyclable items
+    recycle =  sum([1 for i in range(len(data)) if data[i]["Classification"] == 'recycable'])
+    #compostable items
+    compost = sum([1 for i in range(len(data)) if data[i]["Classification"] == 'organic'])
+    #landfill items
+    landfill = sum([1 for i in range(len(data)) if data[i]["Classification"] == 'trash'])
+    #updates sheets
+    update(recycle, compost, landfill)
+    return
 
 #update google sheets
-sheet = client.open("organizedData").sheet1
+def update(r, c, l):
+    sheet = client.open("organizedData").sheet1
+    sheet.update_cell(2,2, r)
+    sheet.update_cell(3,2, c)
+    sheet.update_cell(4,2, l)
+    #print items
+    #recyclable items
+    print('recycable:', r)
+    #compostable items
+    print('compostable:', c)
+    #landfill items
+    print('landfill:', l)
+    return
 
-data2 = sheet.get_all_records()
-
-pprint(data2)
-
-sheet.update_cell(2,2, recycle)
-sheet.update_cell(3,2, compost)
-sheet.update_cell(4,2, landfill)
+count(request())
