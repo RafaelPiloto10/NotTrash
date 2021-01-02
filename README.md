c8e7e5a7b79531cd3fe482880e83690b99666500
Rafael Piloto <31188361+RafaelPiloto10@users.noreply.github.com>
2021-01-02 14:55:35 -0500

    added readme

---
+++ README.md
@@ -0,0 +1,15 @@
+# !Trash
+
+## Inspiration & What it does
+
+## How we built it
+
+## Challenges we ran into
+
+## Accomplishments that we're proud of and what we learned
+
+## What's next for !Trash
+
+## Built With
+
+## Try it out
--- live_prediction.py
+++ live_prediction.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
 
     while(True):
         ret, frame = cap.read()
+        res = np.zeros((224, 224))
         res = cv2.resize(frame, (224, 224), fx=0, fy=0, interpolation = cv2.INTER_CUBIC)
         img = img_to_array(res)
 
@@ -18,28 +19,49 @@ if __name__ == "__main__":
         img = np.expand_dims(img,axis=0)
 
         answer = model.predict(img)
-        print(answer)
-        start_point = (0, 0)
-        end_point = (100, 100)
+        start_point = (frame.shape[1] // 3, frame.shape[0] * 3 // 4)
+        end_point = (frame.shape[1] * 2 // 3, frame.shape[0] * 9 // 10)
+
+
         c = (255, 0, 0)
         thickness = -1
+        category = ""
 
         if answer[0][0] > 0.85:
-            print("The image belongs to Recycle waste category")
             color = (200, 0, 0)
+            category = "Recycle: {:.2f}% Confident".format(answer[0][0] * 100)
         elif answer[0][0] > 0.25:
-            print("The image belongs to Trash waste category")
             c = (0, 0, 200)
+            category = "Landfill: {:.2f}% Confident".format(((answer[0][0] * 100) / 85) * 100)
         else:
-            print("The image belongs to Organic waste category ")
             c = (0, 200, 0)
+            category = "Organic: {:.2f}% Confident".format(100 - answer[0][0] * 100)
 
-        
+        # setup text
+        font = cv2.FONT_HERSHEY_SIMPLEX
+
+        # get boundary of this text
+        textsize = cv2.getTextSize(category, font, 1, 2)[0]
+
+        # get coords based on boundary
+        textX = (frame.shape[1] - textsize[0]) // 2
+        textY = (frame.shape[0] + textsize[1]) * 8 // 10 
+
         frame = cv2.rectangle(frame, start_point, end_point, c, thickness)
-        cv2.imshow('frame', frame)
-        
+    
+        # add text centered on image
+        cv2.putText(frame, category, (textX, textY ), font, 1, (255, 255, 255), 2)
+
+         
+        cv2.imshow('Original', frame)
+        cv2.imshow('Resized', res)
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+        elif cv2.waitKey(1) & 0xFF == ord('b'):
+            _, f = cap.read()
+            background = f
+
 
 
     cap.release()
