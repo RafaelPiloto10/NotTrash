88ee6acc56344e11875545e812546aed35b51031
Rafael Piloto <31188361+RafaelPiloto10@users.noreply.github.com>
2021-01-02 12:18:17 -0500

    updated live prediction view to include feedback

--- live_prediction.py
+++ live_prediction.py
@@ -12,11 +12,6 @@ if __name__ == "__main__":
     while(True):
         ret, frame = cap.read()
         res = cv2.resize(frame, (224, 224), fx=0, fy=0, interpolation = cv2.INTER_CUBIC)
-        cv2.imshow('frame', res)
-        
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
         img = img_to_array(res)
 
         img = img / 255
@@ -24,13 +19,27 @@ if __name__ == "__main__":
 
         answer = model.predict(img)
         print(answer)
+        start_point = (0, 0)
+        end_point = (100, 100)
+        c = (255, 0, 0)
+        thickness = -1
 
         if answer[0][0] > 0.85:
             print("The image belongs to Recycle waste category")
+            color = (200, 0, 0)
         elif answer[0][0] > 0.25:
             print("The image belongs to Trash waste category")
+            c = (0, 0, 200)
         else:
             print("The image belongs to Organic waste category ")
+            c = (0, 200, 0)
+
+        
+        frame = cv2.rectangle(frame, start_point, end_point, c, thickness)
+        cv2.imshow('frame', frame)
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 
     cap.release()
